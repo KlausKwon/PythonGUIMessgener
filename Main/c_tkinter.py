d707955ca64f525576341d941b1316e8bae40d29
KLAUS KWON <39422921+KlausKwon@users.noreply.github.com>
2023-05-08 15:07:35 +0900

    Update c_tkinter.py
    sendbutton -> sendButton

--- Main/c_tkinter.py
+++ Main/c_tkinter.py
@@ -142,7 +142,7 @@ class GUI:
                                 font="Helvetica 10 bold",
                                 width=20,
                                 bg="#ABB2B9",
-                                command=lambda: self.sendbutton(self.entryMsg.get()))
+                                command=lambda: self.sendButton(self.entryMsg.get()))
         
         self.buttonMsg.place(relx=0.77, rely=0.008,
                              relheight=0.06, relwidth=0.22)
